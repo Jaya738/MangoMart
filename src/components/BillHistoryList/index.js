cbbf2511397aec9b4ec95a6fce9cdb89bf37de95
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2021-02-28 02:17:07 +0530

    Implemented search functionality for bills history

--- src/components/BillHistoryList/index.js
+++ src/components/BillHistoryList/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { BillHistoryItem } from "../../components";
 import { getBills, getBillsHistory } from "../../selectors/bills";
+import { get } from "lodash";
 
-function BillHistoryList() {
+function BillHistoryList({ searchInput }) {
   const bills = useSelector(getBills);
   const billsHistory = useSelector(getBillsHistory);
   return (
     <div>
-      {billsHistory.map((eachBill) => (
-        <BillHistoryItem key={eachBill} billId={eachBill} bills={bills} />
-      ))}
+      {billsHistory
+        .filter((eachBill) => {
+          if (searchInput === "") {
+            return eachBill;
+          }
+          const billData = get(bills, `${eachBill}`);
+          if (
+            billData.custName
+              .toLowerCase()
+              .includes(searchInput.toLowerCase()) ||
+            billData.amount.toString().includes(searchInput.toLowerCase())
+          ) {
+            return eachBill;
+          }
+        })
+        .map((eachBill) => (
+          <BillHistoryItem key={eachBill} billId={eachBill} bills={bills} />
+        ))}
     </div>
   );
 }

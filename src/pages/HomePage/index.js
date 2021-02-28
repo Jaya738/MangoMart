028b216a7c3c12fe92e842a20332b2183bc0be36
Jaya Simha Reddy <jayasimhareddy738@gmail.com>
2021-02-28 15:14:38 +0530

    added banner in Home Page

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import features, { flaggedWith } from "../../features";
-import { Header, Footer } from "../../components";
+import { Header, Footer, Wrapper } from "../../components";
 import strings from "../../utils/localization";
+import Banner from "../../assets/Banner.svg";
 
 function Home() {
+  const styles = {
+    banner: {
+      width: "80%",
+      margin: "20px 0px",
+      padding: "16px",
+    },
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  };
   return (
     <div>
       <Header title={strings.appName} />
+      <Wrapper>
+        <div style={styles.container}>
+          <img style={styles.banner} src={Banner} />
+        </div>
+      </Wrapper>
       <Footer />
     </div>
   );
